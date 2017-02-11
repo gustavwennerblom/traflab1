3b41ce7471bfc37e862b6630f35deb33af489bb0
Gustav Wennerblom <gustav.wennerblom@gmail.com>
2017-02-12 03:32:38 +0530

    first working app (unformatted presentation)

--- tweb.py
+++ tweb.py
@@ -3,19 +3,6 @@ from flask import render_template
 
 app=Flask(__name__)
 
-#try:
-#    f=open("traflabkey.txt", "r")
-#    import json
-#    API_KEY=json.loads(self.f.readline())
-#except:
-#    print("Error reading file")
-#    return("FATAL ERROR")
-#finally:
-#    f.close()
-SITE_ID_SICKLAKAJ="1550"
-url="http://api.sl.se/api2/realtimedeparturesV4.{0}?key={1}&siteid={2}&timewindow={3}".format("json", self.API_KEY, self.SITE_ID_SICKLAKAJ, "30")
-
-
 @app.route("/")
 def home():
     str="Hello World"    
@@ -23,12 +10,26 @@ def home():
    
 @app.route('/test')
 def testpage():
-#    return "Test page"
-#    import Traflab
-#    t=Traflab()
-    str="Test page"
-    return render_template('test.html', str=str)
-#    return str(get_trams)
+    try:
+        import os
+        SITE_ROOT=os.path.realpath(os.path.dirname(__file__))
+        path=os.path.join(SITE_ROOT, "traflabkey.txt")
+        f=open(path, "r")
+        import json
+        API_KEY=json.loads(f.readline())
+    except:
+        print("Error reading file")
+        return("FATAL ERROR")
+    finally:
+        f.close()
+    SITE_ID_SICKLAKAJ="1550"
+    url="http://api.sl.se/api2/realtimedeparturesV4.{0}?key={1}&siteid={2}&timewindow={3}".format("json", API_KEY, SITE_ID_SICKLAKAJ, "30")
+    results=get_trams(url)
+       
+    return str(get_trams(url))
+   
+#    str="Test page"
+#    return render_template('test.html', str=str)
 
 def get_full_response(url):
     #Returns a dict from the JSON provided by the Trafiklab API, given a compliant REST call
@@ -38,7 +39,7 @@ def get_full_response(url):
 
 def get_trams(url):
     #Returns a dict of tram destinations (keys) and departure times (values, as displayed on boards)
-    j=self.get_full_response(url)
+    j=get_full_response(url)
     trams=dict()    
     for departure in j["ResponseData"]["Trams"]:
         trams[departure["Destination"]]=departure["DisplayTime"]
